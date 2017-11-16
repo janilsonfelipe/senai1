0debe40ce206e7de8c463998d76da566262a6cbd
janilsonfelipe <janilson_felipe@estudante.sc.senai.br>
2017-11-16 23:43:08 +0000

    adicionando icones

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   get 'pages/home'
-  get 'sobre' => 'pages#about'
+
   get 'test' => 'pages#test'
 
   # The priority is based upon order of creation: first created -> highest priority.
